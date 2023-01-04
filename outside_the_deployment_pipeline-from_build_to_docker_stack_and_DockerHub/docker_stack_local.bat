5177c9832aaa73015bb8d77b8ce5e30546ec6318
jl <jeanlouis.macle@gmail.com>
2023-01-04 15:58:46 -0600

    Minor modification.

--- outside_the_deployment_pipeline-from_build_to_docker_stack_and_DockerHub/docker_stack_local.bat
+++ outside_the_deployment_pipeline-from_build_to_docker_stack_and_DockerHub/docker_stack_local.bat
@@ -1,7 +1,7 @@
 echo off
 
 echo **** Starting the services (backend, PostgreSQL) with docker stack
-docker stack deploy -c ./docker-compose-stack-localImages.yml stack
+docker stack deploy -c ./docker-compose-stack-localImages.yml stack_localImages
 
 start dockerHub_push.bat
 

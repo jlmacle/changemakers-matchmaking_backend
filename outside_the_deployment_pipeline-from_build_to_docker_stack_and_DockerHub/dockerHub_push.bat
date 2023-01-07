67c18a548cdf2d483d735becb98a36b3a16d7d3b
jl <jeanlouis.macle@gmail.com>
2023-01-07 14:20:21 -0600

    Modified the version name to demo0

--- outside_the_deployment_pipeline-from_build_to_docker_stack_and_DockerHub/dockerHub_push.bat
+++ outside_the_deployment_pipeline-from_build_to_docker_stack_and_DockerHub/dockerHub_push.bat
@@ -5,7 +5,7 @@
 echo **** Tagging and pushing the image on DockerHub
 
 :: This version assumes to be signed in on Docker Desktop
-docker tag changemakers-matchmaking-backend:0.0.1-SNAPSHOT jlmacle/changemakers-matchmaking-backend:0.0.1-SNAPSHOT
-docker push jlmacle/changemakers-matchmaking-backend:0.0.1-SNAPSHOT
+docker tag changemakers-matchmaking-backend:demo0 jlmacle/changemakers-matchmaking-backend:demo0
+docker push jlmacle/changemakers-matchmaking-backend:demo0
 
 :: exit

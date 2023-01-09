6cdd52ee3ec58420cd49968695e5be5cc1e9e32c
jl <jeanlouis.macle@gmail.com>
2023-01-09 15:13:58 -0600

    Minor script update

--- outside-the-deployment-pipeline_from-build-to-docker-stack-and-DockerHub/docker_build_by_Spring.bat
+++ outside-the-deployment-pipeline_from-build-to-docker-stack-and-DockerHub/docker_build_by_Spring.bat
@@ -8,7 +8,7 @@ cmd /c "mvn spring-boot:build-image"
 echo **** Removing the jars in the target directory
 del .\target\*.jar*
 
-cd "outside_the_deployment_pipeline-from_build_to_docker_stack_and_DockerHub"
+cd "outside-the-deployment-pipeline_from-build-to-docker-stack-and-DockerHub"
 start docker_stack_local.bat
 
 ::exit
